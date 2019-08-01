7d91d5cccec5cff3ab8807f5e8f9569218f56fb2
Osamu TONOMORI <osamingo@gmail.com>
2019-08-01 19:04:35 +0900

    Simplify bool comparison

--- cmp/internal/value/zero.go
+++ cmp/internal/value/zero.go
@@ -11,7 +11,7 @@ import "reflect"
 func IsZero(v reflect.Value) bool {
 	switch v.Kind() {
 	case reflect.Bool:
-		return v.Bool() == false
+		return !v.Bool()
 	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
 		return v.Int() == 0
 	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
