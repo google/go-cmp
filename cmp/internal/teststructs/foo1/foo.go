d713870ac17fdb9ee5e2ee48ff6562dfb1c0157b
Joe Tsai <joetsai@digital-static.net>
2020-09-23 13:30:54 -0700

    Fix license headers (#236)
    There is no LICENSE.md file, but there is a LICENSE file.

--- cmp/internal/teststructs/foo1/foo.go
+++ cmp/internal/teststructs/foo1/foo.go
@@ -1,6 +1,6 @@
 // Copyright 2020, The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE.md file.
+// license that can be found in the LICENSE file.
 
 // Package foo is deliberately named differently than the parent directory.
 // It contain declarations that have ambiguity in their short names,
